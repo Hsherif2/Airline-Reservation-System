adb7e4c04fd04700e019e3fc5f6e4a61aea5e6a4
Hsherif2 <hsherif2@students.kennesaw.edu>
2024-11-18 18:29:09 -0500

    Update Gui.py
    imported tkinter and created a widget. still need to create more widgets and configure them

--- Gui.py
+++ Gui.py
@@ -1,1 +1,10 @@
+import tkinter as tk
+from datetime import datetime
+
+def create_widgets(self):
+   self.main_menu_frame = tk.Frame(self)
+        self.main_menu_frame.pack()
+self.show_airline_button = tk.Button(self.main_menu_frame, text="Show Airline Schedules", command=self.show_airline, width=20, bg='gray')
+        self.show_airline_button.pack(pady=10)
+
 
